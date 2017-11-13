eecf6849ddab27aa750cbcf1b617f50bc2b3c50e
awoodcroft95 <awoodcroft95@gmail.com>
2017-11-13 13:16:03 +0000

    Added current projects correctly

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            one T = new one();
+            T.mesage();
+            Console.Read();
+        }
+    }
+
+    class one
+    {
+        public void mesage()
+        {
+            Console.Write("Hello my friends");
+        }
+    }
+}
---
+++ ResultsCalculator/ResultsCalculator/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResultsCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Results peter, smith;
+            peter = new Results();
+            smith = new Results();
+            peter.physics(150);
+            peter.chemistry(150);
+            peter.maths(150);
+
+            smith.physics(91);
+            smith.chemistry(91);
+            smith.maths(91);
+
+            peter.showResults();
+            smith.showResults();
+            Console.Read();
+
+            
+        }
+    }
+
+    class Results
+    {
+        private int phy, che, mat;
+        Boolean validInput = true;
+
+        public void physics(int a)
+        {
+            if (a >= 0 && a <= 150)
+            {
+                phy = a;
+            } else
+            {
+                Console.WriteLine("Invalid Physics Mark");
+                validInput = false;
+            }
+        }
+
+        public void chemistry(int b)
+        {
+            if (b >= 0 && b <= 150)
+            {
+                che = b;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Mhemistry Mark");
+                validInput = false;
+            }
+        }
+
+        public void maths(int c)
+        {
+            if (c >= 0 && c <= 150)
+            {
+                mat = c;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Maths Mark");
+                validInput = false;
+            }
+        }
+
+
+        public void showResults()
+        {
+            int total;
+            float per;
+            int numberOfPasses = 0;
+            if (phy > 0 && mat > 0 && che > 0 && validInput)
+            {
+                if (((float)phy * 100 / 150) > 60)
+                {
+                    numberOfPasses++;
+                }
+                if (((float)che * 100 / 150) > 60)
+                {
+                    numberOfPasses++;
+                }
+                if (((float)mat * 100 / 150) > 60)
+                {
+                    numberOfPasses++;
+                }
+
+                if (numberOfPasses == 3)
+                {
+                    total = phy + che + mat;
+                    per = (float)total * 100 / 450;
+
+                    Console.Write("Total marks: " + total + "\n");
+                    Console.Write("Percentage: " + per + "\n");
+                } else if (numberOfPasses == 2)
+                {
+                    Console.WriteLine("Retake exam");
+                } else if (numberOfPasses == 1)
+                {
+                    Console.WriteLine("Repeat the course");
+                }else
+                {
+                    Console.WriteLine("Go home");
+                }
+            } else
+            {
+                Console.Write("No Results Available");
+            }  
+        }
+    }
+}

4566dbe624edb39dbc6cf2bfd3afbd5fa5415697
awoodcroft95 <awoodcroft95@gmail.com>
2017-11-14 08:59:58 +0000

    Finished the reverse vert method

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -13,6 +13,8 @@ namespace Strings
             English e = new English();
             e.words("I am in QA");
             e.find("I am london going to london", "london");
+            e.verticalSentence("I am going to QA");
+            e.reverseVert("I am going to the Moon");
             Console.Read();
         }
     }
@@ -28,7 +30,7 @@ namespace Strings
         public void find(string message, string findWhat)
         {
             int substringLength = findWhat.Length;
-            int foundCount=0;
+            int foundCount = 0;
             for (int i = 0; i < message.Length - substringLength + 1; i++)
             {
                 if (message.Substring(i, substringLength) == findWhat)
@@ -38,6 +40,53 @@ namespace Strings
                 }
             }
             Console.WriteLine(foundCount);
+        }
+
+        public void verticalSentence(string sentence)
+        {
+            List<string> wordArray = new List<string>();
+            string currentWord = "";
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] != (' '))
+                {
+                    currentWord += sentence[i];
+                    if (i == sentence.Length - 1)
+                    {
+                        wordArray.Add(currentWord);
+                    }
+                }
+                else if (sentence[i].Equals(' '))
+                {
+                    wordArray.Add(currentWord);
+                    currentWord = "";
+                }
+            }
+            wordArray.ForEach(Console.WriteLine);
+        }
+
+        public void reverseVert(string sentence)
+        {
+            List<string> wordArray = new List<string>();
+            string currentWord = "";
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] != (' '))
+                {
+                    currentWord += sentence[i];
+                    if (i == sentence.Length - 1)
+                    {
+                        wordArray.Add(currentWord);
+                    }
+                }
+                else if (sentence[i].Equals(' '))
+                {
+                    wordArray.Add(currentWord);
+                    currentWord = "";
+                }
+            }
+            wordArray.Reverse();
+            wordArray.ForEach(Console.WriteLine);
         }
     }
 }

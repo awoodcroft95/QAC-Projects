ca25f658aec47e77522b43e2ae67f52bf4266e54
awoodcroft95 <awoodcroft95@gmail.com>
2017-11-16 17:28:52 +0000

    form with database simple task completed

---
+++ DBWithForm/DBWithForm/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace DBWithForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.t2 = new System.Windows.Forms.TextBox();
+            this.t3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // t1
+            // 
+            this.t1.Location = new System.Drawing.Point(74, 46);
+            this.t1.Name = "t1";
+            this.t1.ReadOnly = true;
+            this.t1.Size = new System.Drawing.Size(100, 20);
+            this.t1.TabIndex = 0;
+            // 
+            // t2
+            // 
+            this.t2.Location = new System.Drawing.Point(74, 83);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(100, 20);
+            this.t2.TabIndex = 1;
+            // 
+            // t3
+            // 
+            this.t3.Location = new System.Drawing.Point(74, 119);
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(100, 20);
+            this.t3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(74, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 215);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Next";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.t3);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.TextBox t2;
+        private System.Windows.Forms.TextBox t3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ DBWithForm/DBWithForm/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DBWithForm
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        bool hasBeenClicked = false;
+        SqlDataReader data;
+
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection();
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename='C:\\Users\\Admin\\Documents\\AWCSharpProjects\\DBWithForm\\DBWithForm\\Database1.mdf';Integrated Security=True";
+            con.Open();
+            cmd = con.CreateCommand();
+            cmd.CommandText = "select * from school";
+            data = cmd.ExecuteReader();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            data.Close();
+            cmd.CommandText = "select isnull(max(Regno), 0)+1 as newR from school";
+            SqlDataReader r = cmd.ExecuteReader();
+            if (r.Read())
+            {
+                string newRegno = r["newR"].ToString();
+                r.Close();
+                string query = $"insert into school values({newRegno}, '{t2.Text}',{t3.Text})";
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+                MessageBox.Show($"New record added to School, with values {newRegno}, {t2.Text}, {t3.Text}");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (data.Read())
+            {
+                t1.Text = data["Regno"].ToString();
+                t2.Text = data["Name"].ToString();
+                t3.Text = data["Marks"].ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (data.Read())
+            {
+                t1.Text = data["Regno"].ToString();
+                t2.Text = data["Name"].ToString();
+                t3.Text = data["Marks"].ToString();
+            }
+        }
+    }
+}

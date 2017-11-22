8fea989d5119a554a2cb50a62d64339e0f2cd61b
awoodcroft95 <awoodcroft95@gmail.com>
2017-11-22 10:47:25 +0000

    Started Some MVC stuff

---
+++ MVCPeople/MVCPeople/Controllers/NamesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPeople.Controllers
+{
+    public class NamesController : Controller
+    {
+        // GET: Names
+        public ActionResult Peter()
+        {
+            return View();
+        }
+
+        public ActionResult Smith()
+        {
+            return View();
+        }
+    }
+}
